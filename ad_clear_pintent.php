89aaabc03044f2b74109a233a1c3cb277f7e6b44
Kevin Wagner <kwagner1312@gmail.com>
2019-03-23 06:32:26 -0500

    Added Save Functionality

--- ad_clear_pintent.php
+++ ad_clear_pintent.php
@@ -21,32 +21,6 @@ if ( is_admin() ) {
     require_once plugin_dir_path( __FILE__ ) . 'admin/admin.php';
     
 }
-
-/**
- * Adding a custom field to Attachment Edit Fields
- * @param  array $form_fields 
- * @param  WP_POST $post        
- * @return array              
- */
-
-    
-
-//        function add_pinterest_fields( $form_fields, $post ) {
-//       
-//            $field_value = get_post_meta( $post->ID, 'pin-description', true );
-//
-//            $form_fields['pin-description'] = array(
-//                'value' => $field_value ? $field_value : '',
-//                'label' => __( 'Pin Description' ),
-//                'helps' => __( 'Add a short description for Pinterest SEO' ),
-//                'input' => 'textarea'
-//            );
-//
-//            return $form_fields;
-//          }
-//        
-//        
-//            add_filter( 'attachment_fields_to_edit', 'add_pinterest_fields', null, 2 );
-      
+     
 
 ?>
--- admin/admin.php
+++ admin/admin.php
@@ -15,13 +15,13 @@ if (! defined( 'ABSPATH' ) ) {
  */
 
     
-
-        function add_pinterest_fields( $form_fields, $post ) {
+        //add attachment fields
+        function ad_add_pinterest_fields( $form_fields, $post ) {
        
             $field_value = get_post_meta( $post->ID, 'pin-description', true );
 
             $form_fields['pin-description'] = array(
-                'value' => $field_value ? $field_value : '',
+                'value' => $field_value ? esc_textarea($field_value) : '',
                 'label' => __( 'Pin Description' ),
                 'helps' => __( 'Add a short description for Pinterest SEO' ),
                 'input' => 'textarea'
@@ -31,6 +31,18 @@ if (! defined( 'ABSPATH' ) ) {
           }
         
         
-            add_filter( 'attachment_fields_to_edit', 'add_pinterest_fields', null, 2 );
+            add_filter( 'attachment_fields_to_edit', 'ad_add_pinterest_fields', null, 2 );
+
+        //save attachment fields
+        function ad_save_pinterest_fields( $attachment_id ) {
+            
+            if ( isset( $_REQUEST['attachments'][$attachment_id]['pin-description'] ) ) {
+                $pinDescription = sanitize_text_field( $_REQUEST['attachments'][$attachment_id]['pin-description'] );
+                update_post_meta( $attachment_id, 'pin-description', $pinDescription );
+            }
+            
+        }
+
+            add_action( 'edit_attachment', 'ad_save_pinterest_fields' );
 
 ?>
